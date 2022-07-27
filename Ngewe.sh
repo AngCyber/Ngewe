c957e3aa53358a6d542950dcb131c7fd3f3a4626
Moch Aang XD <92802033+AngCyber@users.noreply.github.com>
2022-07-28 05:09:29 +0700

    Add files via upload

---
+++ Ngewe.sh
@@ -0,0 +1,41 @@
+echo -e '\033[01;32m'
+echo "--------------------------------"
+echo "         TOOL Ngentod Onlen        "
+echo "          © By : Aang XD      "
+echo "             _Unfaedah_          "
+echo "--------------------------------"
+
+echo -n "Masukan Nama Agan : "
+read lu
+echo -n "Jenis Gaya *contoh : Mengangkang*  :"
+read Gaya
+echo -n "Masukkan Nama Si Korban Yg Mau Di Entot  :"
+read Nama
+echo -n "Masukan Alamat Tempat Untuk Skidipap Nanti :"
+read Alamat
+echo -n "Mau Masukin Dari Atas Atau Dari Bawah? : "
+read Masukin
+echo -n "Mau Punya Anak Berapa?  :"
+read Anak
+echo " Dosa Tanggung Situ Sendiri " | lolcat
+clear
+echo "loading.." | lolcat
+echo "ahhh Ahhh Ahhhh" | lolcat
+echo "Mpsh Ahhhh Ahh" | lolcat
+echo "Ikehh Ahh Ahh. Ayo Kuatin Lagi Bang, Ahh" | lolcat
+echo "ikeh Ikeh. Mpsssss Ahhhh" | lolcat
+echo "Udahh Bang Aku Udh Lemes :(" | lolcat
+echo "Aku Udh Gak Kuat Sayang :(" | lolcat 
+echo
+echo
+echo
+sleep 3
+echo "|=====================================|" | lolcat
+echo "|       Ngentodd Onlen Sukse!!        |" | lolcat
+echo "|=====================================|" | lolcat
+echo 
+echo
+echo "Berhasil!! Anda Telah Punya Anak $Anak . $lu, Si $Nama Minta Di Entot Lagi Katanya Di $Alamat! "
+echo "Pake Gaya $Gaya Katanya"  | lolcat
+echo
+echo "Dosa Tanggung Situ Sendiri :v" | lolcat
